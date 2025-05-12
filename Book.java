cdd3c6e01cf04f792411422a1419cb9da4b13590
sukirthamahalakshmi <143057131+sukirthamahalakshmi@users.noreply.github.com>
2025-05-12 09:55:21 +0530

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,37 @@
+public class Book {
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void borrowBook() {
+        if (isAvailable) {
+            isAvailable = false;
+            System.out.println("You have borrowed the book: " + title);
+        } else {
+            System.out.println("Sorry, the book is not available.");
+        }
+    }
+
+    public void returnBook() {
+        isAvailable = true;
+        System.out.println("You have returned the book: " + title);
+    }
+}
